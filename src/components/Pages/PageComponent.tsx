a08b084184c855aff24dd7f5f49708da03bef355
MerlinDerEchte <merlinraabe@yahoo.de>
2021-11-01 16:10:00 +0100

    as

--- src/components/Pages/PageComponent.tsx
+++ src/components/Pages/PageComponent.tsx
@@ -7,7 +7,7 @@ interface IPageProps {
 export const PageComponent: FC<IPageProps> = ({ title, children }) => {
 
     return(
-            <article className="flex-initial w-full text-xs font-mono flex flex-col overflow-hidden ">
+            <article className="flex-1 w-full text-xs font-mono flex flex-col overflow-hidden ">
                 {children}
             </article>
     )
